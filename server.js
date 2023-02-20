f1626932e7e92ed684c275ee78faea54a4a13eda
BonyaviD <navidprinceofpersia@gmail.com>
2023-02-20 18:34:10 +0330

    change reply markup

--- server.js
+++ server.js
@@ -1,76 +1,64 @@
 const TelegramBot = require("node-telegram-bot-api");
-const token = process.env.TOKEN;
+const token = "5864011130:AAE1ETQGFRsiDHVzJKre1KU0zMdmwmNqxqA";
 const bot = new TelegramBot(token, {polling: true});
 
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(
-    msg.chat.id,
-    " سلام " + msg.chat.first_name + " به ربات خوش آمدید",
-    {
-      reply_markup: {
-        keyboard: [
-          ["خرید", "حجم اکانت"],
-          ["گزارش اکانت", "سوالات متداول", "آموزش"],
-          ["پشتیبانی"],
-        ],
-        resize_keyboard: true,
-      },
-    }
-  );
-});
+  bot.sendMessage(msg.chat.id, " سلام " + msg.chat.first_name + " لطفا ایمیل خود را وارد کنید",)});
 
-bot.on("message", (msg) => {
-  switch (msg.text) {
-    case "خرید":
-      bot.sendMessage(msg.chat.id, "حاوی یک لینک خرید");
-      break;
-    case "حجم اکانت":
-      bot.sendMessage(msg.chat.id, "ایمیل خود را وارد کنید");
-      break;
-    case "آموزش":
-      bot.sendMessage(msg.chat.id, " دیوایس مورد نظر را انتخاب کنید", {
+  bot.on("message", (msg) => {
+    if(msg.text == "email") bot.sendMessage(msg.chat.id, "به پنل خوش آمدید" ,
+      {
         reply_markup: {
-          keyboard: [["Windows", "Android", "iOS"], ["بازگشت"]],
+          keyboard: [["وضعیت اکانت"],
+          ["پشتیبانی", "آموزش", "خرید"]],
           resize_keyboard: true,
         },
-      });
-      break;
-    case "iOS":
-      bot.sendMessage(msg.chat.id, "توضیحات آیفون");
-      break;
-    case "Windows":
-      bot.sendMessage(msg.chat.id, "توضیحات ویندوز");
-      break;
-    case "Android":
-      bot.sendMessage(msg.chat.id, "توضیحات اندروید");
-      break;
-    case "بازگشت":
-      bot.sendMessage(
-        msg.chat.id,
-        " کاربر " + msg.chat.first_name + " گزینه‌ی مورد نظر را انتخاب کنید ",
-        {
+      })
+    else if(msg.text !== "/start" && msg.text !== "email" && msg.text !== "وضعیت اکانت" && msg.text !== "خرید" && 
+    msg.text !== "آموزش" && msg.text !== "پشتیبانی" && msg.text !== "iOS" && msg.text !== "Windows" &&
+    msg.text !== "Android" && msg.text !== "بازگشت" ) bot.sendMessage(msg.chat.id, "ایمیل یافت نشد")
+    
+  })
+
+  bot.on("message", (msg) => {
+    switch (msg.text) {
+      case "وضعیت اکانت":
+        bot.sendMessage(msg.chat.id, "Api");
+        break;
+      case "خرید":
+        bot.sendMessage(msg.chat.id, "  لینک خرید  ");
+        break;
+      case "پشتیبانی":
+        bot.sendMessage(msg.chat.id, "  لینک تلگرام برای پشتیبانی" );
+        break;
+        case "آموزش":
+          bot.sendMessage(msg.chat.id , "لطفا سیستم عام خود رو انتخاب کنید" ,
+         { 
           reply_markup: {
-            keyboard: [
-              ["خرید", "حجم اکانت"],
-              ["گزارش اکانت", "سوالات متداول", "آموزش"],
-              ["پشتیبانی"],
-            ],
+            keyboard: [["Windows", "Android", "iOS"], ["بازگشت"]],
             resize_keyboard: true,
           },
-        }
-      );
-      break;
-    case "سوالات متداول":
-      bot.sendMessage(msg.chat.id, "متنی در مورد سوالات متداول");
-      break;
-    case "گزارش اکانت":
-      bot.sendMessage(msg.chat.id, "گزارش به کاربر");
-      break;
-    case "پشتیبانی":
-      bot.sendMessage(
-        msg.chat.id,
-        "برای پشتیبانی میتوانید با آیدی زیر ارتباط برقرار کنید "
-      );
-      break;
-  }
-});
+          });
+          break;
+      case "iOS":
+        bot.sendMessage(msg.chat.id, "توضیحات آیفون");
+        break;
+      case "Windows":
+        bot.sendMessage(msg.chat.id, "توضیحات ویندوز");
+        break;
+      case "Android":
+        bot.sendMessage(msg.chat.id, "توضیحات اندروید");
+        break;
+      case "بازگشت":
+        bot.sendMessage(
+          msg.chat.id,
+          " کاربر " + msg.chat.first_name + " گزینه‌ی مورد نظر را انتخاب کنید ",
+          {
+            reply_markup: {
+              keyboard: [["وضعیت اکانت"],
+              ["پشتیبانی", "آموزش", "خرید"]],
+              resize_keyboard: true,
+            },
+          }
+        );
+    break; }})
